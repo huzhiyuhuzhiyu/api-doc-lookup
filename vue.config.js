f5d13efa5d604c84aee66fe71ceb8065f559b7bb
DESKTOP-5E76NPE\tiger <1909446527@qq.com>
2024-06-06 08:54:26 +0800

    本地环境不需要去除log

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const path = require('path')
 const defaultSettings = require('./src/settings.js')
 const define = require('./src/utils/define.js')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
@@ -67,15 +66,6 @@ module.exports = {
       new MonacoWebpackPlugin({
         // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
         languages: ['javascript', 'css', 'html', 'typescript', 'json', 'java', 'sql']
-      }),
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-            pure_funcs: ['console.log']
-          }
-        }
       })
     ]
   },

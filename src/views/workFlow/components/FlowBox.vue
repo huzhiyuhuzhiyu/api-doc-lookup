d7c7cc846ae89eef0d59e0cd99d013f890ed2ac8
ZuRenZhao <arencn@163.com>
2024-10-18 14:12:58 +0800

    -- 修改流程对应form

--- src/views/workFlow/components/FlowBox.vue
+++ src/views/workFlow/components/FlowBox.vue
@@ -337,8 +337,8 @@ export default {
         'b046': 'warehouseManagement/finishedProductWarehouseManagement/inventoryList/Form.vue',
         'b048': 'esop/fileUpload/workinginstruction/Form.vue',
         'b049': 'esop/fileUpload/workinginstruction/Form.vue',
-        'b050': 'esop/fileUpload/docment/Form.vue',
-        'b052': 'esop/fileUpload/docment/Form.vue',
+        'b050': 'esop/fileUpload/workinginstruction/Form.vue',
+        'b052': 'esop/fileUpload/workinginstruction/Form.vue',
         'b051': 'warehouseManagement/finishedProductWarehouseManagement/transferManagement/Form.vue',
       },
       inspectionTypeList: [

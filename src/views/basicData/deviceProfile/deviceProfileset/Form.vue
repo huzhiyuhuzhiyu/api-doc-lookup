1a0b7920870c0d0c18b4ebeed0d2291a2474426f
PC-20230701YAHO\tang <2460855246@qq.com>
2024-11-28 13:40:02 +0800

    ++

--- src/views/basicData/deviceProfile/deviceProfileset/Form.vue
+++ src/views/basicData/deviceProfile/deviceProfileset/Form.vue
@@ -19,7 +19,7 @@
                     <el-col :sm="8" :xs="24" v-if="isProjectSwitch=='1'">
                       <el-form-item label="所属项目" prop="projectId">
                         <el-select v-model="dataForm.projectId" placeholder="请选择所属项目" :loading="loadingprojectId" :disabled="type !== 'add'" style="width: 100%;">
-                          <el-option v-for="item in projectIdList" :key="item.code" :label="item.name" :value="item.code"></el-option>
+                          <el-option v-for="item in projectIdList" :key="item.id" :label="item.name" :value="item.id"></el-option>
                         </el-select>
                       </el-form-item>
                     </el-col>
@@ -326,6 +326,7 @@ export default {
       picArr: [],
       type: '',
       dataForm: {
+        projectId: '',
         repairUserId: '',
         salespersonId: "",
         purchaseAmount: "",

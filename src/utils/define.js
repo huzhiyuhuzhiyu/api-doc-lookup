9317c115f7467df6e67f770e390cf65f22748701
DESKTOP-HUSIMON\chenzhu <1120480450@qq.com>
2024-12-26 18:56:59 +0800

    ++

--- src/utils/define.js
+++ src/utils/define.js
@@ -1,6 +1,6 @@
 // 开发环境接口配置
-// const APIURl = 'http://api.test.zgt.nbjuxuan.com/' 
-const  APIURl="http://192.168.110.61:30004" 
+const APIURl = 'http://api.test.zgt.nbjuxuan.com/' 
+// const  APIURl="http://192.168.110.61:30004" 
 module.exports = {
   APIURl: APIURl,
   timeout: process.env.NODE_ENV === 'development' ? 1000000 : 1000000,

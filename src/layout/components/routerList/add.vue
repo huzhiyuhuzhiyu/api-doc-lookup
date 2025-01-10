54666a005082dfa05ee0052db23c59c1a14c76ff
dongkechao13587810217@163.com <13587810217@163.com>
2025-01-10 08:47:43 +0800

    中亚路由收藏

--- src/layout/components/routerList/add.vue
+++ src/layout/components/routerList/add.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-dialog title="收藏" :visible.sync="addVisible" width="30%" :close-on-click-modal="false"
-      :close-on-press-escape="false" :show-close="false">
+      :close-on-press-escape="false" :show-close="false" class="JNPF-dialog JNPF-dialog_center selectPro">
       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="60px" style="margin: 0 10px;">
         <el-form-item label="名称" prop="urlName">
           <el-input v-model="ruleForm.urlName"></el-input>
@@ -15,18 +15,14 @@
         <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
       </span>
     </el-dialog>
-
-
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import { addUserFavorites } from '@/api/permission/user'
-import dragDialog from "@/directive/el-drag-dialog";
-import { getIMReply, deleteChatRecord, relocation } from '@/api/system/message'
 export default {
-  name: 'RouterList',
+  name: 'addList',
   components: {},
   data() {
     return {
@@ -35,51 +31,22 @@ export default {
         urlAddress: ''
       },
       rules: {
-        urlName: [
-          { required: true, message: '请输入名称', trigger: 'blur' },
-        ],
-
+        urlName: [{ required: true, message: '请输入名称', trigger: 'blur' }]
       },
       top: 0,
       left: 0,
       selectedTag: {},
       visitedViews: '',
       addVisible: false,
-      activeTab: 'reply',
-      userList: [],
-      replyList: [],
-      loading: false,
-      replyLoading: false,
-      visibleMenu: false,
-      visible: false,
-      finish: false,
-      listQuery: {
-        keyword: '',
-        currentPage: 1,
-        pageSize: 20
-      }
-    }
-  },
-  created() {
 
-  },
-  computed: {
-    ...mapGetters(['userInfo']),
-  },
-  watch: {
-    activeTab(val) {
-      if (val === 'contacts' && !this.userList.length) {
-        this.getUserList()
-      }
-    },
-    visibleMenu(value) {
-      if (value) {
-        document.body.addEventListener('click', this.closeMenu)
-      } else {
-        document.body.removeEventListener('click', this.closeMenu)
-      }
+      loading: false
     }
   },
+  created() { },
+  computed: {
+    ...mapGetters(['userInfo'])
+  },
+  watch: {},
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
@@ -105,347 +72,28 @@ export default {
           //   });
           // });
           this.ruleForm.userId = this.userInfo.userId
-          addUserFavorites(this.ruleForm).then(res => {
+          addUserFavorites(this.ruleForm).then((res) => {
             this.addVisible = false
             this.$message({
               type: 'success',
               message: '新建成功!'
-            });
+            })
           })
         } else {
-          console.log('error submit!!');
-          return false;
+          console.log('error submit!!')
+          return false
         }
-      });
-    },
-    enterRouter() {
-      this.drawer = false
-      this.$router.push({
-        path: "/basicData/supplierManagement/supplierProfile",
-
       })
     },
-    closeMenu() {
-      this.visibleMenu = false
-    },
-    openMenu(tag, e) {
-      const menuMinWidth = 105
-      const offsetLeft = this.$el.getBoundingClientRect().left // container margin left
-      const offsetWidth = this.$el.offsetWidth // container width
-      const maxLeft = offsetWidth - menuMinWidth // left boundary
-      const left = e.clientX - offsetLeft + 30 // 15: margin right
-      if (left > maxLeft) {
-        this.left = maxLeft
-      } else {
-        this.left = left
-      }
-      this.top = e.clientY - 95
-      this.visibleMenu = true
-      this.selectedTag = tag
-    },
-
 
     init() {
       this.finish = false
       this.addVisible = true
-      this.listQuery.currentPage = 1
-      this.listQuery.keyword = ''
-      this.activeTab = 'reply'
+
       this.ruleForm.urlName = this.$router.currentRoute.meta.zhTitle
       this.ruleForm.urlAddress = window.location.pathname
       console.log(this.ruleForm, 'jjj')
-      this.userList = []
-      this.replyList = []
-      this.getReplyList()
-      this.$nextTick(() => {
-        this.bindScroll()
-      })
-    },
-    bindScroll() {
-      let _this = this,
-        vBody = _this.$refs.userList;
-      vBody.addEventListener("scroll", function () {
-        if (vBody.scrollHeight - vBody.clientHeight - vBody.scrollTop <= 300 && !_this.loading && !_this.finish) {
-          _this.listQuery.currentPage += 1
-          _this.getUserList()
-        }
-      });
-    },
-    getReplyList() {
-      this.replyLoading = true
-      getIMReply().then(res => {
-        this.replyList = res.data.list
-        this.replyLoading = false
-      })
-    },
-    getUserList() {
-      this.loading = true
-      getImUser(this.listQuery).then(res => {
-        if (res.data.list.length < this.listQuery.pageSize) this.finish = true
-        this.userList = [...this.userList, ...res.data.list]
-        this.loading = false
-      })
-    },
-    search() {
-      this.userList = []
-      this.finish = false
-      this.listQuery.currentPage = 1
-      this.listQuery.pageSize = 20
-      this.getUserList()
-    },
-
-    toggleBlink() {
-      const boo = this.replyList.some(o => o.unreadMessage)
-      this.$emit('changeTwinkle', boo)
-    },
-    readInfo(item, isReply) {
-      let socket = this.$store.getters.socket
-      let updateReadMessage = {
-        method: "UpdateReadMessage",
-        formUserId: item.id,
-        token: this.$store.getters.token
-      }
-      socket.send(JSON.stringify(updateReadMessage));
-      if (isReply) {
-        item.unreadMessage = 0
-      } else {
-        for (let i = 0; i < this.replyList.length; i++) {
-          if (item.id === this.replyList[i].id) {
-            this.replyList[i].unreadMessage = 0
-            break
-          }
-        }
-      }
-      this.toggleBlink()
-      this.$refs.JNPFIm.list = []
-      //获取消息列表
-      var messageList = {
-        method: "MessageList",
-        toUserId: item.id,
-        formUserId: this.userInfo.userId,
-        token: this.$store.getters.token,
-        currentPage: 1,
-        pageSize: 30,
-        sord: "desc"
-      }
-      socket.send(JSON.stringify(messageList));
-      this.$refs.JNPFIm.init(item)
-    },
-    getMsgText(text, type) {
-      let message = ''
-      switch (type) {
-        case 'voice':
-          message = '[语音]'
-          break;
-        case 'image':
-          message = '[图片]'
-          break;
-        default:
-          message = text
-          break;
-      }
-      return message
-    },
-    handleClose(done) {
-      let vBody = this.$refs.userList;
-      vBody.removeEventListener("scroll", function () { });
-      done();
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.contextmenu {
-  margin: 0;
-  background: #fff;
-  z-index: 3000;
-  position: absolute;
-  list-style-type: none;
-  padding: 5px 0;
-  border-radius: 4px;
-  font-size: 12px;
-  font-weight: 400;
-  color: #333;
-  box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, 0.3);
-
-  li {
-    margin: 0;
-    padding: 7px 16px;
-    cursor: pointer;
-
-    &:hover {
-      background: #eee;
-    }
-  }
-}
-
-.contacts-drawer {
-  .el-tabs {
-    height: 100%;
-
-    .el-tabs__header {
-      margin-bottom: 0 !important;
-    }
-
-    .el-tabs__item {
-      width: 50%;
-      text-align: center;
-      color: #303133 !important;
-    }
-
-    .el-tabs__nav {
-      width: 100%;
-    }
-
-    .el-tabs__content {
-      height: calc(100% - 40px);
-    }
-
-    .el-tab-pane {
-      height: 100%;
-      overflow: hidden;
-    }
-  }
-
-  .noData-txt {
-    font-size: 14px;
-    color: #909399;
-    line-height: 20px;
-    text-align: center;
-    padding-top: 20px;
-  }
-
-  .search-input {
-    .el-input__inner {
-      border-radius: 0;
-      border-right: none;
-      border-left: none;
-      border-top: none;
-    }
-
-    .el-input__icon {
-      cursor: pointer;
-
-      &:hover {
-        color: #1890ff;
-      }
-    }
-  }
-
-  .el-drawer__header {
-    border: none;
-  }
-
-  .el-drawer__body {
-    overflow: hidden;
-
-    .userList {
-      overflow: auto;
-      overflow-x: hidden;
-      position: relative;
-      height: calc(100% - 32px);
-
-      &.replyList {
-        height: 100%;
-
-        .title {
-          overflow: auto;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          height: 20px;
-        }
-      }
-
-      .el-badge {
-        margin-top: 10px;
-      }
-
-      .el-loading-mask {
-        top: 100px;
-      }
-    }
-
-    .userList-item {
-      position: relative;
-      display: block;
-      padding: 0 20px;
-      background-color: #fff;
-      border-bottom: 1px solid #f5f7f9;
-      height: 60px;
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-
-      .offLine {
-        filter: grayscale(1);
-      }
-
-      &:hover {
-        background-color: #f5f7f9;
-      }
-
-      .userList-txt {
-        margin-left: 14px;
-        overflow: hidden;
-        flex: 1;
-        padding-top: 1px;
-
-        .title {
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          overflow: hidden;
-          font-size: 14px;
-          color: #333;
-          margin-bottom: 5px;
-          line-height: 20px;
-        }
-
-        .name {
-          font-size: 12px;
-          color: #999;
-
-          .content {
-            display: inline-block;
-            width: 120px;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-          }
-
-          .time {
-            float: right;
-          }
-        }
-      }
-    }
-  }
-}
-</style>
-<style scoped>
-.el-input__inner {
-  font-size: inherit !important;
-}
-
-.el-drawer__body {
-  padding: 0;
-}
-
-/deep/.el-drawer__header {
-  border: none;
-}
-
-/deep/ .el-tabs__item {
-  width: 50%;
-  text-align: center;
-}
-
-/deep/.el-tabs__nav {
-  width: 100%;
-}
-
-/deep/.el-tabs__active-bar {
-  width: 120px;
-}
-</style>
--- src/layout/components/routerList/routerList.vue
+++ src/layout/components/routerList/routerList.vue
@@ -1,43 +1,37 @@
 <template>
   <div>
-    <el-dialog title="收藏夹" :visible.sync="dialogVisible" width="60%" :fullscreen="editShow" :close-on-click-modal="false"
-      :close-on-press-escape="false" :show-close="false">
-      <template v-if="editShow">
-        <el-table :data="userFavoritesList" style="width: 100%;height: 77vh;" v-loading="loading">
-          <el-table-column prop="urlName" label="名称" min-width="180">
-            <template slot-scope="scope">
-              <el-input @change="switchShow(scope.row, 'sort')" @blur="urlNameBlur(scope.row)"
-                v-model="scope.row.urlName"></el-input>
-            </template>
-          </el-table-column>
-          <el-table-column prop="urlAddress" label="地址" min-width="180">
-          </el-table-column>
-          <el-table-column fixed="right" label="操作" width="80" align="center">
-            <template slot-scope="scope">
-              <tableOpts :hasEdit="false" @del="handleDel(scope.row.id)">
-              </tableOpts>
-            </template>
-          </el-table-column>
-        </el-table>
-        <pagination :total="total" :page.sync="listQuery.pageNum" :limit.sync="listQuery.pageSize"
-          @pagination="initData" />
-      </template>
-
-      <template v-else>
-        <div style="display: flex;flex-wrap: wrap;">
-          <div v-for="item in userFavoritesList" :key="item.id">
-            <div style="margin: 10px;">
-              <el-button type="primary" round @click="enterRouter(item.urlAddress)">
-                {{ item.urlName }}
-              </el-button>
+    <el-dialog title="收藏夹" :visible.sync="dialogVisible" width="60%" :close-on-click-modal="false"
+      :close-on-press-escape="false" :show-close="false" class="JNPF-dialog JNPF-dialog_center selectPro">
+      <el-dialog title="收藏修改" :visible.sync="innerVisible" width="30%" :close-on-click-modal="false"
+        :close-on-press-escape="false" :show-close="false" class="JNPF-dialog JNPF-dialog_center selectPro"
+        append-to-body>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="60px" style="margin: 0 10px;">
+          <el-form-item label="名称" prop="urlName">
+            <el-input v-model="ruleForm.urlName"></el-input>
+          </el-form-item>
+          <el-form-item label="路由">
+            <el-input v-model="ruleForm.urlAddress"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="innerVisible = false">取 消</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+        </span>
+      </el-dialog>
+      <div class="card-list" v-loading="listLoading">
+        <div v-for="(line, index) in userFavoritesList" :key="line.id">
+          <el-card shadow="hover" class="box-card">
+            <div @click="enterRouter(line.urlAddress)"> {{ line.urlName }} </div>
+            <div style="text-align:right">
+              <el-button size="mini" type="text" @click="editSate(line)">编辑</el-button>
+              <el-button size="mini" type="text" class="JNPF-table-delBtn" @click="handleDel(line.id)">删除</el-button>
             </div>
-          </div>
+          </el-card>
         </div>
 
-      </template>
+      </div>
 
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="changeState">{{ editShow ? '路由跳转' : '编辑' }}</el-button>
         <el-button @click="dialogVisible = false">取 消</el-button>
       </span>
     </el-dialog>
@@ -53,15 +47,28 @@ export default {
   data() {
     return {
       dialogVisible: false,
+      innerVisible: false,
+      listLoading: false,
       loading: false,
       menuVisible: false,
       userFavoritesList: [],
-      editShow: false,
       listQuery: {
         keyword: '',
         pageNum: 1,
         pageSize: 5
-      }
+      },
+      ruleForm: {
+        urlName: '',
+        urlAddress: ''
+      },
+      rules: {
+        urlName: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+        ],
+        urlAddress: [
+          { required: true, message: '请输入地址', trigger: 'blur' },
+        ],
+      },
     }
   },
   created() { },
@@ -76,29 +83,26 @@ export default {
         path: urlAddress
       })
     },
-    changeState() {
-      this.editShow = !this.editShow
-    },
-    switchShow(row) {
-      if (!row.urlName) return this.$message.error('请修改名称')
-      let obj = row
-      updateUserFavorites(obj)
-        .then((response) => {
-          this.$message({
-            message: '修改成功',
-            type: 'success',
-            duration: 1500,
-            onClose: () => {
-              this.getUserFavoritesList()
-            }
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          updateUserFavorites(this.ruleForm).then(res => {
+            this.innerVisible = false
+            this.getUserFavoritesList()
+            this.$message({
+              type: 'success',
+              message: '修改成功!'
+            });
           })
-        })
-        .catch(() => {
-          this.btnLoading = false
-        })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
     },
-    urlNameBlur(row) {
-      if (!row.urlName) this.getUserFavoritesList()
+    editSate(row) {
+      this.ruleForm = { ...row }
+      this.innerVisible = true
     },
     handleDel(id) {
       this.$confirm(this.$t("common.delTip"), this.$t("common.tipTitle"), {
@@ -121,13 +125,10 @@ export default {
     init() {
       this.finish = false
       this.dialogVisible = true
-      this.editShow = false
       this.listQuery.pageNum = 1
       this.listQuery.keyword = ''
       this.listQuery.userId = this.userInfo.userId
-      this.activeTab = 'reply'
       this.userFavoritesList = []
-      this.replyList = []
       this.getUserFavoritesList()
     },
 
@@ -135,16 +136,10 @@ export default {
       this.loading = true
       getUserFavorites(this.listQuery).then((res) => {
         this.userFavoritesList = res.data.records
+
         this.loading = false
       })
     },
-    search() {
-      this.userFavoritesList = []
-      this.finish = false
-      this.listQuery.pageNum = 1
-      this.listQuery.pageSize = 20
-      this.getUserFavoritesList()
-    }
   }
 }
 </script>
@@ -317,64 +312,87 @@ export default {
   }
 }
 </style>
-<style scoped>
-.el-input__inner {
-  font-size: inherit !important;
-}
+<style lang="scss" scoped>
+.card-list {
+  display: flex;
+  flex-wrap: wrap;
 
-.el-drawer__body {
-  padding: 0;
-}
+  .box-card-add {
+    ::v-deep .el-card__body {
+      display: flex;
+      align-items: center;
+    }
+  }
 
-/deep/.el-drawer__header {
-  border: none;
-}
+  .box-card {
+    width: 260px;
+    height: 80px;
+    box-sizing: border-box;
+    margin: 5px 8px 0 0;
+    position: relative;
+    font-size: 18px;
+    cursor: pointer;
 
-/deep/ .el-tabs__item {
-  width: 50%;
-  text-align: center;
-}
+    ::v-deep .el-card__body {
+      position: relative;
+      height: 100%;
 
-/deep/.el-tabs__nav {
-  width: 100%;
-}
+      div {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        // overflow: hidden;
+      }
+    }
 
-/deep/.el-tabs__active-bar {
-  width: 120px;
-}
-</style>
-<style>
-.contextmenu__item {
-  display: block;
-  line-height: 34px;
-  text-align: center;
-}
+    .icon-checked {
+      display: block;
+      width: 12px;
+      height: 12px;
+      border: 12px solid #409eff;
+      border-left: 12px solid transparent;
+      border-top: 12px solid transparent;
+      border-bottom-right-radius: 4px;
+      position: absolute;
+      right: 0px;
+      bottom: 0px;
 
-.contextmenu__item:not(:last-child) {
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-}
+      i {
+        position: absolute;
+        top: -2px;
+        left: -2px;
+        font-size: 12px;
+        color: #fff;
+      }
+    }
 
-.menu {
-  position: absolute;
-  background-color: #fff;
-  width: 100px;
-  /*height: 106px;*/
-  font-size: 12px;
-  color: #444040;
-  border-radius: 4px;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  border-radius: 3px;
-  border: 1px solid rgba(0, 0, 0, 0.15);
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
-  white-space: nowrap;
-  z-index: 1000;
-}
+    &.box-card-active {
+      background-color: #3fb9f8;
+      color: #fff;
+    }
 
-.contextmenu__item:hover {
-  cursor: pointer;
-  background: #66b1ff;
-  border-color: #66b1ff;
-  color: #fff;
+    .template-item__delete-btn {
+      display: none;
+      position: absolute;
+      top: -3px;
+      right: -3px;
+      width: 20px;
+      height: 20px;
+      padding: 2px;
+      transition: display .3s linear;
+
+      .icon-delete {
+        width: 16px;
+        height: 16px;
+        background: url(~@/assets/images/delete.png) center center no-repeat no-repeat;
+        background-size: cover;
+      }
+    }
+
+    &:hover {
+      .template-item__delete-btn {
+        display: block;
+      }
+    }
+  }
 }
 </style>

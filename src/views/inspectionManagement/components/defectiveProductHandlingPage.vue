60d1a271ca9b74fc783e7b5a93163718c22b94de
dongkechao13587810217@163.com <13587810217@163.com>
2025-02-27 14:18:52 +0800

    不良品处理单-动态产品名称问题修复

--- src/views/inspectionManagement/components/defectiveProductHandlingPage.vue
+++ src/views/inspectionManagement/components/defectiveProductHandlingPage.vue
@@ -70,12 +70,10 @@
             <el-table-column prop="inspectionOrderNo" label="检验单号" min-width="200" sortable="custom" />
             <el-table-column prop="projectName" label="所属项目" width="120"
               v-if="isProjectSwitch === '1'"></el-table-column>
-            <el-table-column prop="productName" label="产品名称" width="120"
-              v-if="isProductNameSwitch === '1'"></el-table-column>
-            <el-table-column prop="productDrawingNo" label="品名规格" min-width="180" sortable="custom" />
+            <el-table-column prop="productCode" label="产品编码" min-width="180" sortable="custom" />
             <el-table-column prop="productName" label="产品名称" width="160" v-if="isProductNameSwitch === '1'"
               show-overflow-tooltip></el-table-column>
-            <el-table-column prop="productCode" label="产品编码" min-width="180" sortable="custom" />
+            <el-table-column prop="productDrawingNo" label="品名规格" min-width="180" sortable="custom" />
             <el-table-column prop="productCategoryName" label="产品分类" width="160" sortable="custom" />
 
             <el-table-column prop="inspectionDate" label="检验日期" width="120" sortable="custom" />

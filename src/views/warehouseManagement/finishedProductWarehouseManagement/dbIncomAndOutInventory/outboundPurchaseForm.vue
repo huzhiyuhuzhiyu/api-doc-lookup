ec9aa97a9756dd52a15ab24d4f1574d4f32e2e45
DESKTOP-HUSIMON\chenzhu <1120480450@qq.com>
2025-03-20 15:48:35 +0800

    采购退货出库 拦截处理

--- src/views/warehouseManagement/finishedProductWarehouseManagement/dbIncomAndOutInventory/outboundPurchaseForm.vue
+++ src/views/warehouseManagement/finishedProductWarehouseManagement/dbIncomAndOutInventory/outboundPurchaseForm.vue
@@ -1217,7 +1217,7 @@ export default {
                 this.$message.error("产品信息第" + (index + 1) + "行数量不能为空或为0")
                 break
               }
-              if (item.num > item.availableBatchNumber) {
+              if (Number(item.num) > Number(item.availableBatchNumber)) {
                 submitFlag = false
                 this.$message.error("产品信息第" + (index + 1) + "行数量不能超过批次库存数量")
                 break

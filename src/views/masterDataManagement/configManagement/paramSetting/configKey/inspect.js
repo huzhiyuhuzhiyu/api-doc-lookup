9839f5132e3a932f2e3fb3ebbeab33b1025209ea
dongkechao13587810217@163.com <13587810217@163.com>
2025-03-22 10:32:09 +0800

    业务参数设置增加报工检验设置

--- src/views/masterDataManagement/configManagement/paramSetting/configKey/inspect.js
+++ src/views/masterDataManagement/configManagement/paramSetting/configKey/inspect.js
@@ -3,6 +3,10 @@ export default {
         configKeyLabel: '自动评审',
         description: '自动评审'
     },
+    work_report_inspect: {
+        configKeyLabel: '报工检验',
+        description: '启用后，在质量管理的报工检验待检验单菜单列表中对报工记录进行检验。'
+    },
     procure_inspection: {
         configKeyLabel: '启用采购检验人员设置',
         description: '启用后，在质量管理的采购检验待检收货单菜单列表只能显示单前检验人员的采购收货单。',

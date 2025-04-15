0158904b77988cb6cb3d84f389c4cf2df9836673
dongkechao13587810217@163.com <13587810217@163.com>
2025-04-15 11:17:29 +0800

    生产任务拆分功能

--- src/views/productionManagement/ringPlan/ringTaskManagement/index.vue
+++ src/views/productionManagement/ringPlan/ringTaskManagement/index.vue
@@ -93,7 +93,7 @@
             <el-table-column prop="mainUnit" label="单位" width="80" />
             <el-table-column prop="productionQuantity" label="总生产数量" min-width="140" sortable="custom" />
             <el-table-column prop="completedQuantity" label="已完成数量" min-width="140" sortable="custom" />
-            <el-table-column prop="splitQuantity" label="已拆分数量" min-width="140" sortable="custom" />
+            <el-table-column prop="splitQuantity" label="已拆分数量" min-width="140" sortable="custom" v-has="'btn_split'" />
             <el-table-column prop="prodSchedule" label="完成进度" min-width="140">
               <template slot-scope="scope">
                 <el-progress

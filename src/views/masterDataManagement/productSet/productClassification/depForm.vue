799cfa3134f47078acf745483c4f78151f68237b
dongkechao13587810217@163.com <13587810217@163.com>
2025-04-21 08:35:16 +0800

    主数据选择产品分类增加   type: 'material' 传参

--- src/views/masterDataManagement/productSet/productClassification/depForm.vue
+++ src/views/masterDataManagement/productSet/productClassification/depForm.vue
@@ -74,7 +74,8 @@ export default {
     return {
       requestObjTwo: {
         pageSize: -1,
-        classAttribute: ''
+        classAttribute: '',
+        type: 'material',
       },
       title: '',
       getcategoryTree,

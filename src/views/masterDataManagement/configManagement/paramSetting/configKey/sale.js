cf097df660f56f7ba7f4627a0a1ccb0bf46017d7
dongkechao13587810217@163.com <13587810217@163.com>
2025-04-22 17:03:56 +0800

    增加业务参数设置启用客户产品修改同步销售订单

--- src/views/masterDataManagement/configManagement/paramSetting/configKey/sale.js
+++ src/views/masterDataManagement/configManagement/paramSetting/configKey/sale.js
@@ -3,4 +3,8 @@ export default {
         configKeyLabel: '启用所属销售可见',
         description: '启用所属销售可见后，销售管理只能展示客户配置所属销售的人员可见。'
     },
+    cpp_rw_order: {
+        configKeyLabel: '启用客户产品修改同步销售订单',
+        description: '启用客户产品修改同步销售订单后，修改客户产品时,将该客户产品销售订单(未完成、未转计划)里的信息同步修改'
+    },
 }

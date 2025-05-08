88e05e573ab3c3f3c1920affc2494948760b5d18
DESKTOP-HUSIMON\chenzhu <1120480450@qq.com>
2025-05-08 15:09:04 +0800

    结存

--- src/views/warehouseManagement/inventoryInquiry/cornerContactBalanceReport/grinderLineStockBalance/index.vue
+++ src/views/warehouseManagement/inventoryInquiry/cornerContactBalanceReport/grinderLineStockBalance/index.vue
@@ -298,6 +298,7 @@ export default {
       
       this.isProjectSwitchFlag = true
     this.$nextTick(function () {
+      this.search('basic', 'search')
   
       // this.getShelvesName()
     })

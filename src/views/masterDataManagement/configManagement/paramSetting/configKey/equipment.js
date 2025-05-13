7cdb382955bfc61bbd28709a04a67f8075b03096
DESKTOP-HUSIMON\chenzhu <1120480450@qq.com>
2025-05-13 14:32:55 +0800

    +++

--- src/views/masterDataManagement/configManagement/paramSetting/configKey/equipment.js
+++ src/views/masterDataManagement/configManagement/paramSetting/configKey/equipment.js
@@ -1,9 +1,9 @@
 export default {
-    spot_check_name: {
+    inspectionUsersetting: {
         configKeyLabel: '仅查看自己的点检任务',
         description: '如果勾选，则点检任务列表只展示当前登录账户的任务'
     },
-    maintenance_name: {
+    maintenanceUsersetting: {
         configKeyLabel: '仅查看自己的保养任务',
         description: '如果勾选，则保养任务列表只展示当前登录账户的任务'
     },

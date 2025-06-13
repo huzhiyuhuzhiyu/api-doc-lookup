7f9c165a027c20b8e2dd66bd194f56272cef61e8
DESKTOP-HUSIMON\chenzhu <1120480450@qq.com>
2025-06-13 09:44:30 +0800

    调整批量检验功能

--- src/views/inspectionManagement/reportWorkInspection/inspectionNotice.vue
+++ src/views/inspectionManagement/reportWorkInspection/inspectionNotice.vue
@@ -587,6 +587,8 @@ export default {
 						productsId: item.productsId,
 						ratio: item.ratio,
 						remark: item.remark,
+            approvalStatus:'ok',
+
 						// unqualifiedQuantity: this.inspectionResults === 'qualified' ?
 						// 	0 : Number(item.qualifiedQuantity) + Number(item
 						// 		.unqualifiedQuantity)
--- src/views/inspectionManagement/salesReturnInspection/inspectionNotice.vue
+++ src/views/inspectionManagement/salesReturnInspection/inspectionNotice.vue
@@ -598,6 +598,8 @@ export default {
 						productsId: item.productsId,
 						ratio: item.ratio,
 						remark: item.remark,
+            approvalStatus:'ok',
+            
 						// unqualifiedQuantity: this.inspectionResults === 'qualified' ?
 						// 	0 : item.deliveryQuantity
 					}

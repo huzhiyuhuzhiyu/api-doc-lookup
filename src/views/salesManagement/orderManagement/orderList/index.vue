d505e24b3b495fdac83e379025febe8816095e8a
Paxinuo <15521753071@163.com>
2025-12-09 09:10:54 +0800

    dake

--- src/views/salesManagement/orderManagement/orderList/index.vue
+++ src/views/salesManagement/orderManagement/orderList/index.vue
@@ -1,10 +1,10 @@
 <script>
 import SuperQuery from '@/components/SuperQuery/index.vue'
 
-import {buttonList, getColumns} from "./data";
-import {deleteOrders, getsaleOrderList} from "@/api/salesManagement/assemblyOrders";
-import {getOrdersConfirmed, getOrdersConfirmedIssuance, ordersFeedbackDeliveryFinished} from "@/api/salesManagement/orderChanges";
-import {getPrintBusInfo} from "@/api/system/printDev";
+import { buttonList, getColumns } from "./data";
+import { deleteOrders, getsaleOrderList } from "@/api/salesManagement/assemblyOrders";
+import { getOrdersConfirmed, getOrdersConfirmedIssuance, ordersFeedbackDeliveryFinished } from "@/api/salesManagement/orderChanges";
+import { getPrintBusInfo } from "@/api/system/printDev";
 import Form from './Form.vue'
 import PrintDialog from '@/components/no_mount/printDialog/index.vue';
 import BatchPrintBrowse from "@/components/PrintBrowse/BatchPrintBrowse.vue";
@@ -19,6 +19,35 @@ export default {
   },
   data() {
     return {
+      systemSearchView: [{
+        matchLogic: "AND", // 条件逻辑（固定）*
+        fullName: "默认视图", // 视图名称*
+        conditionJson: { // 视图内容配置*
+          condition: [ // 视图查询条件（自动根据绑定表格的列顺序排序）
+            // 这里放置系统原顶栏显示的查询元素，如：
+            // {
+            //   prop: 'createTime', // 属性*
+            //   value: [this.jnpf.getToday('YYYY-MM-DD HH:mm:ss', 'today-29'), this.jnpf.getToday('YYYY-MM-DD HH:mm:ss', 'todayLastMoment')], // 默认值
+            //   symbol: 'between', // 比较符*
+            //   timeOffset: true, // 保存视图后的静态时间区间随实际查询时刻偏移
+            //   fixed: true // 是否在搜索栏显示
+            // },
+            { prop: 'orderNo', symbol: 'like', fixed: true },
+          ],
+          // keywordQuery: this.jnpf.getKeywordQuery('product'), // 带有产品信息的表使用此预设
+          pageSize: 20, // 每页条数*
+          orderItems: [
+            {
+              asc: false,
+              column: ''
+            },
+            {
+              asc: false,
+              column: 'createTime'
+            }
+          ]
+        },
+      }],
       loading: false,
       visible: false,
       printVisible: false,
@@ -44,24 +73,6 @@ export default {
           options: this.global.salesOrderState
         },
       ],
-      initListQuery: {
-        orderNo: '',
-        cooperativePartnerName: '',
-        cooperativePartnerCode: '',
-        orderItems: [
-          {
-            asc: false,
-            column: ''
-          },
-          {
-            asc: false,
-            column: 'create_time'
-          }
-        ],
-        superQuery: {},
-        pageNum: 1,
-        pageSize: 20
-      },
       listQuery: {},
       btnList: buttonList,
       columnList: [],
@@ -73,22 +84,22 @@ export default {
         confirms: true,
         listMethod: getOrdersConfirmed,
         tableItems: [
-          {prop: 'procurementPending', label: '采购待审', sortable: 'custom', align: 'center', minWidth: '120px'},
-          {prop: 'productName', label: '产品名称', minWidth: '220px', sortable: 'custom'},
-          {prop: 'productCode', label: '产品编码', sortable: 'custom'},
-          {prop: 'drawingNo', label: '产品型号', minWidth: '220px', sortable: 'custom'},
-          {prop: 'customerProductDrawingNo', label: '客户型号', minWidth: '220px', sortable: 'custom'},
-          {prop: 'num', label: '订单数量', sortable: 'custom', minWidth: '120px', align: 'center'},
-          {prop: 'stockInventoryNum', label: '总库存', sortable: 'custom', minWidth: '90px', align: 'center'},
-          {prop: 'unpickedNum', label: '仓库未拣货数', sortable: 'custom', minWidth: '150px', align: 'center'},
-          {prop: 'unDeliveryNum', label: '未发数', sortable: 'custom', minWidth: '90px', align: 'center'},
-          {prop: 'onlineNum', label: '在线数', sortable: 'custom', minWidth: '90px', align: 'center'},
-          {prop: 'inTransitNum', label: '在途数', sortable: 'custom', minWidth: '90px', align: 'center'},
-          {prop: 'actualInventoryNum', label: '实际库存', sortable: 'custom', minWidth: '120px', align: 'center'},
-          {prop: 'availableInventoryNum', label: '可用库存', sortable: 'custom', minWidth: '120px', align: 'center'},
-          {prop: 'confirmedStatus', label: '状态', sortable: 'custom', minWidth: '220px', align: 'center'},
-          {prop: 'inventoryArrangementNum', label: '库存安排', sortable: 'custom', minWidth: '160px', align: 'center'},
-          {prop: 'orderPoolNum', label: '订单池', sortable: 'custom', minWidth: '160px', align: 'center'}
+          { prop: 'procurementPending', label: '采购待审', sortable: 'custom', align: 'center', minWidth: '120px' },
+          { prop: 'productName', label: '产品名称', minWidth: '220px', sortable: 'custom' },
+          { prop: 'productCode', label: '产品编码', sortable: 'custom' },
+          { prop: 'drawingNo', label: '产品型号', minWidth: '220px', sortable: 'custom' },
+          { prop: 'customerProductDrawingNo', label: '客户型号', minWidth: '220px', sortable: 'custom' },
+          { prop: 'num', label: '订单数量', sortable: 'custom', minWidth: '120px', align: 'center' },
+          { prop: 'stockInventoryNum', label: '总库存', sortable: 'custom', minWidth: '90px', align: 'center' },
+          { prop: 'unpickedNum', label: '仓库未拣货数', sortable: 'custom', minWidth: '150px', align: 'center' },
+          { prop: 'unDeliveryNum', label: '未发数', sortable: 'custom', minWidth: '90px', align: 'center' },
+          { prop: 'onlineNum', label: '在线数', sortable: 'custom', minWidth: '90px', align: 'center' },
+          { prop: 'inTransitNum', label: '在途数', sortable: 'custom', minWidth: '90px', align: 'center' },
+          { prop: 'actualInventoryNum', label: '实际库存', sortable: 'custom', minWidth: '120px', align: 'center' },
+          { prop: 'availableInventoryNum', label: '可用库存', sortable: 'custom', minWidth: '120px', align: 'center' },
+          { prop: 'confirmedStatus', label: '状态', sortable: 'custom', minWidth: '220px', align: 'center' },
+          { prop: 'inventoryArrangementNum', label: '库存安排', sortable: 'custom', minWidth: '160px', align: 'center' },
+          { prop: 'orderPoolNum', label: '订单池', sortable: 'custom', minWidth: '160px', align: 'center' }
         ],
         listRequestObj: '',
         listDataFormatting: (res) => {
@@ -99,26 +110,28 @@ export default {
     }
   },
   created() {
-    this.listQuery = JSON.parse(JSON.stringify(this.initListQuery))
-    this.initData()
+
   },
   methods: {
-    async initData() {
+    async initData(listQuery) {
+      if (listQuery) this.listQuery = listQuery;
+      if (!this.listQuery?.pageSize) return this.$message.error('请先等待视图加载完成！');
+      const listLoadKey = this.listLoadKey = +new Date();
+      if (listLoadKey !== this.listLoadKey) return; // 请求过期
+
       this.loading = true
       try {
         const res = await getsaleOrderList(this.listQuery);
-        const {total, records} = res.data
+        const { total, records } = res.data
         this.tableData = records;
         this.total = total
       } finally {
         this.loading = false
       }
     },
-
     closePrint() {
       this.printVisible = false
     },
-
     printView(row, enCode, fullName) {
       this.selectArr = [row]
       this.enCode = enCode
@@ -128,7 +141,6 @@ export default {
         this.$refs.printTemplate.init(enCode)
       })
     },
-
     async printOrder(enCode) {
       try {
         const res = await getPrintBusInfo(enCode)
@@ -141,17 +153,15 @@ export default {
           id: id
         }))
         this.$refs.batchPrint.print(printData);
-      } catch (e) {
+      } catch ( e ) {
       }
     },
-
     handleChangeStatus(val, row) {
     },
     calcInventoryArrangementNum() {
     },
     calcOrderPoolNum() {
     },
-
     async submitAllProduct(data) {
       const params = data.map(item => ({
         ...item,
@@ -165,7 +175,6 @@ export default {
         this.$message.error(response.msg || '确认失败')
       }
     },
-
     validateSelectedRows() {
       if (!this.selectedRow.length) {
         this.$message.warning('请至少选择一条数据');
@@ -177,9 +186,8 @@ export default {
       }
       return true;
     },
-
     handleButtonClick(type) {
-      switch (type) {
+      switch ( type ) {
         case 'add':
           this.visible = true
           this.$nextTick(() => {
@@ -199,7 +207,6 @@ export default {
         default:
       }
     },
-
     async handleConfirmDeliveryDate() {
       try {
         await this.$confirm('确定要执行此操作吗？', '提示', {
@@ -211,15 +218,15 @@ export default {
           await ordersFeedbackDeliveryFinished(this.selectedRow[0].id)
           this.$message.success('确认成功')
           await this.initData()
-        } catch (e) {
+        } catch ( e ) {
           this.$message.success('确认失败')
         }
-      } catch (error) {
+      } catch ( error ) {
       }
     },
 
     handleColumnClick(row, type) {
-      switch (type) {
+      switch ( type ) {
         case 'look':
         case 'edit':
         case 'copy':
@@ -240,7 +247,7 @@ export default {
         type: 'warning'
       }).then(async () => {
         const res = await deleteOrders(id);
-        const {msg} = res
+        const { msg } = res
         if (msg === 'Success') {
           this.$message.success('删除成功')
           this.initData()
@@ -256,36 +263,15 @@ export default {
       this.initData()
     },
 
-    sortChange({prop, order}) {
-      let newProp = ''
-      if (prop === 'createTime') {
-        newProp = prop
-      } else {
-        newProp = prop.replace(/[A-Z]/g, (match) => '_' + match.toLowerCase())
-      }
-      this.listQuery.orderItems[0].asc = order === 'ascending'
-      this.listQuery.orderItems[0].column = order === null ? '' : newProp
-      this.initData()
-    },
+
     columnSetFun() {
       this.$refs.dataTable.showDrawer()
     },
     getAlign(align) {
       return align || 'left'
     },
-    superQuerySearch(query) {
-      this.listQuery.superQuery = query
-      this.superQueryVisible = false
-      this.search()
-    },
-    search() {
-      this.initData()
-    },
-    reset() {
-      this.$refs['dataTable'].$refs.JNPFTable.clearSort() // 清除排序箭头高亮
-      this.listQuery = JSON.parse(JSON.stringify(this.initListQuery))
-      this.search()
-    },
+
+
   }
 }
 </script>
@@ -293,40 +279,7 @@ export default {
 <template>
   <div class="JNPF-common-layout">
     <div class="JNPF-common-layout-center  JNPF-flex-main">
-      <el-row class="JNPF-common-search-box" :gutter="16" style="margin-bottom: 5px !important;">
-        <el-form @submit.native.prevent @keyup.enter.native="search()">
-          <el-col :span="4">
-            <el-form-item>
-              <el-input v-model.trim="listQuery.orderNo"
-                placeholder="订单号"
-                clearable/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="4">
-            <el-form-item>
-              <el-input v-model.trim="listQuery.cooperativePartnerCode"
-                placeholder="客户编码"
-                clearable/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="4">
-            <el-form-item>
-              <el-input v-model.trim="listQuery.cooperativePartnerName"
-                placeholder="客户名称"
-                clearable/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="6">
-            <el-form-item>
-              <el-button size="mini" type="primary" icon="el-icon-search"
-                @click="search()">查询
-              </el-button>
-              <el-button size="mini" icon="el-icon-refresh-right" @click="reset()">重置
-              </el-button>
-            </el-form-item>
-          </el-col>
-        </el-form>
-      </el-row>
+      <JNPF-tableQuery :listQuery="listQuery" :systemSearchView="systemSearchView" tableRef="dataTable"/>
       <div class="JNPF-common-layout-main JNPF-flex-main">
         <div class="JNPF-common-head" style="padding: 8px">
           <div class="JNPF-common-head-left">
@@ -336,31 +289,30 @@ export default {
             />
           </div>
           <div class="JNPF-common-head-right">
-            <el-tooltip content="高级查询" placement="top" v-if="true">
-              <el-link icon="icon-ym icon-ym-filter JNPF-common-head-icon" :underline="false"
-                @click="superQueryVisible = true"/>
+            <el-tooltip effect="dark" content="数据排序设置" placement="top">
+              <el-link icon="icon-ym icon-ym-generator-flow JNPF-common-head-icon" :underline="false"
+                       @click="$refs.dataTable.showSortDrawer()"/>
             </el-tooltip>
             <el-tooltip effect="dark" :content="$t('common.columnSettings')" placement="top">
               <el-link icon="icon-ym icon-ym-shezhi JNPF-common-head-icon" :underline="false"
-                @click="columnSetFun()"/>
+                       @click="columnSetFun()"/>
             </el-tooltip>
             <el-tooltip effect="dark" :content="$t('common.refresh')" placement="top">
               <el-link icon="icon-ym icon-ym-Refresh JNPF-common-head-icon" :underline="false"
-                @click="initData()"/>
+                       @click="initData()"/>
             </el-tooltip>
           </div>
         </div>
         <JNPF-table customKey="orderList"
-          v-loading="loading"
-          :data="tableData"
-          :has-c="true"
-          @selection-change="(val) => selectedRow = val"
-          :row-key="'id'"
-          fixedNO
-          :setColumnDisplayList="columnList"
-          @sort-change="sortChange"
-          ref="dataTable"
-          custom-column>
+                    v-loading="loading"
+                    :data="tableData"
+                    :has-c="true"
+                    @selection-change="(val) => selectedRow = val"
+                    :row-key="'id'"
+                    fixedNO
+                    :setColumnDisplayList="columnList"
+                    ref="dataTable"
+                    custom-column :listQuery="listQuery" @queryChange="initData" :queryJson="superQueryJson">
           <template v-for="column in columnsConfig">
             <el-table-column
               v-if="typeof column.show === 'function' ? column.show() : true"
@@ -387,11 +339,11 @@ export default {
           <el-table-column label="操作" width="180" fixed="right">
             <template slot-scope="{ row }">
               <el-button size="mini" type="text" :disabled="row.documentStatus !== 'draft'"
-                @click="handleColumnClick(row, 'edit')">
+                         @click="handleColumnClick(row, 'edit')">
                 编辑
               </el-button>
               <el-button class="JNPF-table-delBtn" size="mini" type="text" :disabled="row.documentStatus !== 'draft'"
-                @click="handleColumnClick(row, 'delete')">
+                         @click="handleColumnClick(row, 'delete')">
                 删除
               </el-button>
               <el-dropdown hide-on-click>
@@ -405,7 +357,7 @@ export default {
                     复制
                   </el-dropdown-item>
                   <el-dropdown-item :disable="row.documentStatus === 'draft'"
-                    @click.native="printView(row,'p002','销售单打印')">
+                                    @click.native="printView(row,'p002','销售单打印')">
                     打印
                   </el-dropdown-item>
                   <el-dropdown-item @click.native="handleColumnClick(row, 'look')">
@@ -416,8 +368,7 @@ export default {
             </template>
           </el-table-column>
         </JNPF-table>
-        <pagination :total="total" :page.sync="listQuery.pageNum" :limit.sync="listQuery.pageSize"
-          @pagination="initData"
+        <pagination :total="total" :page.sync="listQuery.pageNum" :limit.sync="listQuery.pageSize" @pagination="initData()"
         />
       </div>
     </div>
@@ -442,14 +393,9 @@ export default {
         <div></div>
       </template>
     </ComSelect-page>
-    <!-- 高级查询 -->
-    <SuperQuery partentOrChild="orderListSuperQuery" :show="superQueryVisible" ref="SuperQuery"
-      table-ref="dataTable"
-      :columnOptions="superQueryJson"
-      @superQuery="superQuerySearch" @close="superQueryVisible = false"/>
     <Form ref="Form" v-if="visible" @close="close"/>
     <PrintDialog :visible.sync="printVisible" @closePrint="closePrint" @printSubmit="printOrder"
-      :printQuery="printQuery" :enCode="enCode" ref="printTemplate"/>
+                 :printQuery="printQuery" :enCode="enCode" ref="printTemplate"/>
     <BatchPrintBrowse ref="batchPrint" :fullName="fullName"/>
   </div>
 </template>

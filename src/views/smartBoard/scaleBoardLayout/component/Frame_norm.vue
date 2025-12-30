3b81034c34cb02cce889a06f7ce4c332f6e6208e
Carrey <2954831281@qq.com>
2025-12-30 15:39:27 +0800

    成品仓库日常管理看板 质量frame是日质量合格率

--- src/views/smartBoard/scaleBoardLayout/component/Frame_norm.vue
+++ src/views/smartBoard/scaleBoardLayout/component/Frame_norm.vue
@@ -1,6 +1,6 @@
 <script>
 import FrameLayout from '@/views/smartBoard/scaleBoardLayout/component/FrameLayout.vue'
-import { getScreenProdAchievementData, getScreenReworkData } from '@/api/smartBoard'
+import { getScreenDayQualifiedData, getScreenProdAchievementData, getScreenReworkData } from '@/api/smartBoard'
 import Bus from '@/views/smartBoard/util/Bus.js'
 import ScreenTable from "@/views/smartBoard/scaleBoardLayout/component/ScreenTable.vue";
 
@@ -64,6 +64,53 @@ export default {
             }), {
               norm: '产量达成率',
               target: '98%',
+            })
+          ]
+
+          this.tableColumns.unshift(
+            {
+              prop: 'norm',
+              label: '指标',
+              width: '70px',
+            },
+            {
+              prop: 'target',
+              label: '目标',
+              width: '35px',
+              color: '#8FA1FF',
+            })
+
+        }).finally(err => {
+          if (loadingFlag) Bus.$emit('subLoading')
+        })
+      } if (this.title === '质量' && this.query.productionLineName === '成品仓库日常管理看板') {
+        getScreenDayQualifiedData({
+          tenantId: 'nm',
+          _title: this.title,
+          startDate: this.jnpf.getToday('YYYY-MM-DD', 'today-7'),
+          endDate: this.jnpf.getToday('YYYY-MM-DD', 'today-1'),
+          // productionLineName: '大线装配车间',
+          ...this.query,
+        }).then(res => {
+          this.viewData = res.data || []
+          this.tableColumns = res.data.map(item => ({
+            prop: item.name,
+            label: item.name,
+            color: (row) => {
+              const value = Number(row[item.name].replace('%', ''))
+              const targetValue = Number(row.target.replace('%', ''))
+              if (targetValue * 0.9 > value) return '#dc3545'
+              if (targetValue * 0.95 > value) return '#FCAA47'
+              return '#fff'
+            },
+          }))
+          this.viewData = [
+            res.data.reduce((prev, cur) => ({
+              ...prev,
+              [cur.name]: this.jnpf.numberFormat(cur.targetValue, 2) + '%',
+            }), {
+              norm: '日质量合格率',
+              target: '95%',
             })
           ]
 

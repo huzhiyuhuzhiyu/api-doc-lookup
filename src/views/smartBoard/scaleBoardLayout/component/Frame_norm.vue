672a83e6be4f9e3041a09211b09953cfca7fe059
Carrey <2954831281@qq.com>
2026-01-12 16:34:12 +0800

    达克大屏补充数据

--- src/views/smartBoard/scaleBoardLayout/component/Frame_norm.vue
+++ src/views/smartBoard/scaleBoardLayout/component/Frame_norm.vue
@@ -1,6 +1,6 @@
 <script>
 import FrameLayout from '@/views/smartBoard/scaleBoardLayout/component/FrameLayout.vue'
-import { getScreenDayQualifiedData, getScreenProdAchievementData, getScreenReworkData } from '@/api/smartBoard'
+import { getScreenCostData, getScreenDayQualifiedData, getScreenProdAchievementData, getScreenQualifiedData, getScreenReworkData } from '@/api/smartBoard'
 import Bus from '@/views/smartBoard/util/Bus.js'
 import ScreenTable from "@/views/smartBoard/scaleBoardLayout/component/ScreenTable.vue";
 
@@ -23,6 +23,7 @@ export default {
   data() {
     return {
       viewData: [],
+      reworkData: null,
       tableColumns: [],
       staticColumns: [
         {
@@ -37,6 +38,12 @@ export default {
           color: '#8FA1FF',
         },
       ],
+      targetComparatorList: [
+        { label: "大于", value: "gt", type: "primary", symbol: '>' },
+        { label: "小于", value: "lt", type: "warning", symbol: '<' },
+        { label: "小于等于", value: "le", type: "warning", symbol: '<=' },
+        { label: "大于等于", value: "ge", type: "primary", symbol: '>=' }
+      ],
     }
   },
   mounted() {
@@ -49,110 +56,73 @@ export default {
   methods: {
     async getData(loadingFlag = false) {
       if (loadingFlag) Bus.$emit('addLoading')
+      const query = {
+        tenantId: 'nm',
+        _title: this.title,
+        startDate: this.jnpf.getToday('YYYY-MM-DD', 'today-7'),
+        endDate: this.jnpf.getToday('YYYY-MM-DD', 'today-1'),
+        ...this.query,
+      }
       if (this.title === '产量') {
-        getScreenProdAchievementData({
-          tenantId: 'nm',
-          _title: this.title,
-          startDate: this.jnpf.getToday('YYYY-MM-DD', 'today-7'),
-          endDate: this.jnpf.getToday('YYYY-MM-DD', 'today-1'),
-          // productionLineName: '大线装配车间',
-          ...this.query,
-        }).then(res => {
-          this.viewData = res.data || []
-          this.tableColumns = res.data[0].list.map(item => ({
-            prop: item.name,
-            label: item.name,
-            color: (row) => {
-              const value = Number(row[item.name].toString().replace('%', ''))
-              const targetValue = Number(row.target.toString().replace('%', ''))
-              if (targetValue * 0.9 > value) return '#dc3545'
-              if (targetValue * 0.95 > value) return '#FCAA47'
-              return '#fff'
-            },
-          }))
-
-          this.viewData = res.data.map(item => ({
-            norm: item.name,
-            target: item.targetValue,
-            ...item.list.reduce((prev, cur) => ({
-              ...prev,
-              [cur.name]: this.jnpf.numberFormat(cur.targetValue, 2),
-            }), {}),
-          }))
-
-        }).finally(err => {
+        getScreenProdAchievementData(query).then(this.successCallback).finally(err => {
           if (loadingFlag) Bus.$emit('subLoading')
         })
-      } if (this.title === '质量' && this.query.productionLineName === '成品仓库') {
-        getScreenDayQualifiedData({
-          tenantId: 'nm',
-          _title: this.title,
-          startDate: this.jnpf.getToday('YYYY-MM-DD', 'today-7'),
-          endDate: this.jnpf.getToday('YYYY-MM-DD', 'today-1'),
-          // productionLineName: '大线装配车间',
-          ...this.query,
-        }).then(res => {
-          this.viewData = res.data || []
-          this.tableColumns = res.data.map(item => ({
-            prop: item.name,
-            label: item.name,
-            color: (row) => {
-              const value = Number(row[item.name].replace('%', ''))
-              const targetValue = Number(row.target.replace('%', ''))
-              if (targetValue * 0.9 > value) return '#dc3545'
-              if (targetValue * 0.95 > value) return '#FCAA47'
-              return '#fff'
-            },
-          }))
-          this.viewData = [
-            res.data.reduce((prev, cur) => ({
-              ...prev,
-              [cur.name]: this.jnpf.numberFormat(cur.targetValue, 2) + '%',
-            }), {
-              norm: '日质量合格率',
-              target: '95%',
-            })
-          ]
-
-        }).finally(err => {
+      }
+      if (this.title === '质量' && this.query.productionLineName === '成品仓库') {
+        getScreenDayQualifiedData(query).then(this.successCallback).finally(err => {
           if (loadingFlag) Bus.$emit('subLoading')
         })
-      } else if (this.title === '质量' || this.title === '成本') {
-        getScreenReworkData({
-          tenantId: 'nm',
-          _title: this.title,
-          startDate: this.jnpf.getToday('YYYY-MM-DD', 'today-7'),
-          endDate: this.jnpf.getToday('YYYY-MM-DD', 'today-1'),
-          // productionLineName: '大线装配车间',
-          ...this.query,
-        }).then(res => {
-          this.viewData = res.data || []
-          this.tableColumns = res.data.map(item => ({
-            prop: item.name,
-            label: item.name,
-            color: (row) => {
-              const value = Number(row[item.name].replace('%', ''))
-              const targetValue = Number(row.target.replace('%', ''))
-              if (targetValue * 0.9 < value) return '#dc3545'
-              if (targetValue * 0.95 < value) return '#FCAA47'
-              return '#fff'
-            },
-          }))
-          this.viewData = [
-            res.data.reduce((prev, cur) => ({
-              ...prev,
-              [cur.name]: this.jnpf.numberFormat(cur.targetValue, 2) + '%',
-            }), {
-              norm: '返工率',
-              target: '2%',
-            })
-          ]
+      } else if (this.title === '质量') {
+        getScreenQualifiedData(query).then(this.successCallback).finally(err => {
+          if (loadingFlag) Bus.$emit('subLoading')
+        })
 
-        }).finally(err => {
+        Bus.$emit('addLoading')
+        this.reworkData = {
+          jnpf: this.jnpf,
+          targetComparatorList: this.targetComparatorList,
+          tableColumns: [],
+          viewData: []
+        }
+        getScreenReworkData(query).then(res => this.successCallback(res, this.reworkData)).finally(err => {
+          if (loadingFlag) Bus.$emit('subLoading')
+        })
+      } else if (this.title === '成本') {
+        getScreenCostData(query).then(this.successCallback).finally(err => {
           if (loadingFlag) Bus.$emit('subLoading')
         })
       }
     },
+    successCallback(res, target) {
+      const _this = target || this
+      if(!Array.isArray(res.data)) {
+        res.data = [res.data]
+      }
+      _this.tableColumns = res.data[0].list.map(item => ({
+        prop: item.name,
+        label: item.name,
+        color: (row) => {
+          const value = Number(row[item.name].toString().replace('%', ''))
+          const targetValue = Number(row.target.toString().replace('%', ''))
+          if (eval(`${value}
+          ${row.symbol}
+          ${targetValue}`)) return row.meetTargetColor
+          else return row.notMeetTargetColor
+        },
+      }))
+
+      _this.viewData = res.data.map(item => ({
+        norm: item.name,
+        target: (item.indicatorRules.targetValue || 0) + (item.name.endsWith('率') ? '%' : ''),
+        symbol: this.targetComparatorList.find(item2 => item2.value === item.indicatorRules.targetComparator)?.symbol,
+        meetTargetColor: item.indicatorRules.meetTargetColor,
+        notMeetTargetColor: item.indicatorRules.notMeetTargetColor,
+        ...item.list.reduce((prev, cur) => ({
+          ...prev,
+          [cur.name]: this.jnpf.numberFormat(cur.targetValue, 2) + (item.name.endsWith('率') ? '%' : ''),
+        }), {}),
+      }))
+    }
   }
 }
 </script>
@@ -161,6 +131,7 @@ export default {
   <FrameLayout :title="title" :subTitle="subTitle">
     <div class="container">
       <ScreenTable :data="viewData" :columns="staticColumns.concat(tableColumns)" />
+      <ScreenTable v-if="reworkData" :data="reworkData.viewData" :columns="staticColumns.concat(reworkData.tableColumns)" />
     </div>
   </FrameLayout>
 </template>
